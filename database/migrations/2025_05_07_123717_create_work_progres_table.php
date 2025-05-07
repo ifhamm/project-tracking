853876f4ae9229586b7432b2af0b7c3d659ac948
Shendyrf <154389023+Shendyrf@users.noreply.github.com>
2025-05-07 21:03:13 +0700

    update database
    remove table customer and add new table work_progres

---
+++ database/migrations/2025_05_07_123717_create_work_progres_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_progres', function (Blueprint $table) {
+            $table->uuid('id_progres')->primary();
+            $table->uuid('no_iwo');
+            $table->foreign('no_iwo')->references('no_iwo')->on('parts');
+            $table->string('step_order');
+            $table->string('step_name');
+            $table->boolean('is_complete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_progres');
+    }
+};
